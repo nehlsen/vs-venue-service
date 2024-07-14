ff5d29037244e1e360dd14c41b958101299b30e7
Nils Weigel <nils.weigel@gmail.com>
2024-07-14 18:18:50 +0200

    fixed http response code

--- src/main/java/org/nehlsen/venueshot/venueservice/Controller/VenueController.java
+++ src/main/java/org/nehlsen/venueshot/venueservice/Controller/VenueController.java
@@ -2,10 +2,12 @@ package org.nehlsen.venueshot.venueservice.Controller;
 
 import org.nehlsen.venueshot.venueservice.Entity.Venue;
 import org.nehlsen.venueshot.venueservice.Repository.VenueRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -26,6 +28,7 @@ public class VenueController {
     }
 
     @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
     public Venue createVenue(@RequestBody Venue venue) {
         // TODO validate venue
         return venueRepository.save(venue);
